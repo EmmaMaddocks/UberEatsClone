389014ee47deb9d227da8a179fcc5237d53642b3
Emma Maddocks <emma.l.maddocks@gmail.com>
2022-12-22 14:02:02 +0000

    header tabs, categories complete

---
+++ components/Categories.js
@@ -0,0 +1,61 @@
+import { View, Text, Image, ScrollView } from "react-native";
+import React from "react";
+
+const items = [
+	{
+		image: require("../assets/images/shopping-bag.png"),
+		text: "Pick-up",
+	},
+	{
+		image: require("../assets/images/soft-drink.png"),
+		text: "Soft Drinks",
+	},
+	{
+		image: require("../assets/images/deals.png"),
+		text: "Deals",
+	},
+	{
+		image: require("../assets/images/coffee.png"),
+		text: "Coffee and Tea",
+	},
+	{
+		image: require("../assets/images/desserts.png"),
+		text: "Desserts",
+	},
+	{
+		image: require("../assets/images/fast-food.png"),
+		text: "Fast Food",
+	},
+	{
+		image: require("../assets/images/bread.png"),
+		text: "Bakery",
+	},
+];
+
+export default function Categories() {
+	return (
+		<View
+			style={{
+				backgroundColor: "white",
+				paddingVertical: 10,
+				paddingLeft: 20,
+			}}
+		>
+			<ScrollView horizontal showsHorizontalScrollIndicator={false}>
+				{items.map((item, index) => (
+					<View key={index} style={{ alignItems: "center", marginRight: 30 }}>
+						<Image
+							source={item.image}
+							style={{
+								width: 50,
+								height: 40,
+								resizeMode: "container",
+							}}
+						/>
+						<Text style={{ fontSize: 13, fontWeight: "900" }}>{item.text}</Text>
+					</View>
+				))}
+			</ScrollView>
+		</View>
+	);
+}
---
+++ components/HeaderTabs.js
@@ -0,0 +1,50 @@
+import { View, Text, Button, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+
+export default function HeaderTabs() {
+	const [activeTab, setActiveTab] = useState("Delivery");
+
+	return (
+		<View style={{ flexDirection: "row", alignSelf: "center" }}>
+			<HeaderButton
+				text="Delivery"
+				btnColor="black"
+				textColor="white"
+				activeTab={activeTab}
+				setActiveTab={setActiveTab}
+			/>
+
+			<HeaderButton
+				text="Pickup"
+				btnColor="white"
+				textColor="black"
+				activeTab={activeTab}
+				setActiveTab={setActiveTab}
+			/>
+		</View>
+	);
+}
+
+const HeaderButton = (props) => (
+	<TouchableOpacity
+		style={{
+			backgroundColor: props.activeTab === props.text ? "black" : "white",
+			paddingVertical: 6,
+			paddingHorizontal: 16,
+			borderRadius: 30,
+		}}
+		onPress={() => {
+			props.setActiveTab(props.text);
+		}}
+	>
+		<Text
+			style={{
+				color: props.activeTab === props.text ? "white" : "black",
+				fontSize: 15,
+				fontWeight: "500",
+			}}
+		>
+			{props.text}
+		</Text>
+	</TouchableOpacity>
+);
